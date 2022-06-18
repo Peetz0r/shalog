8c76cfa7948ddd09de554778de8ccd64a2463f63
Peetz0r <peter@haas-en-berg.nl>
2022-06-18 17:51:42 +0200

    Initial mqtt stats script

--- stats.py
+++ stats.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-import glob, json, pprint
+import glob, json, pprint, paho.mqtt.publish
 
-files = glob.glob('../2017-db/*.json')
-# ~ files = glob.glob('db/*.json')
+# ~ files = glob.glob('../2017-db/*.json')
+files = glob.glob('db/*.json')
 
 numTotalItems = 0
 numUnusedItems = 0
@@ -46,3 +46,21 @@ print(f'numCurrentLoans  =  {numCurrentLoans}')
 print(f'numTotalAngels   =  {numTotalAngels}')
 print(f'numActiveAngels  =  {numActiveAngels}')
 print(f'numBusyAngels    =  {numBusyAngels}')
+
+with open('mqtt-login.json') as f:
+  j = json.load(f)
+  pprint.pprint(j)
+
+  print(j['user'])
+
+  msgs = [
+    {'topic': j['topicPrefix'] + '/numTotalItems', 'payload': numTotalItems},
+    {'topic': j['topicPrefix'] + '/numUnusedItems', 'payload': numUnusedItems},
+    {'topic': j['topicPrefix'] + '/numTotalLoans', 'payload': numTotalLoans},
+    {'topic': j['topicPrefix'] + '/numCurrentLoans', 'payload': numCurrentLoans},
+    {'topic': j['topicPrefix'] + '/numTotalAngels', 'payload': numTotalAngels},
+    {'topic': j['topicPrefix'] + '/numActiveAngels', 'payload': numActiveAngels},
+    {'topic': j['topicPrefix'] + '/numBusyAngels', 'payload': numBusyAngels}
+  ]
+
+  paho.mqtt.publish.multiple(msgs, hostname=j['host'], auth={'username': j['user'], 'password': j['pass']})
