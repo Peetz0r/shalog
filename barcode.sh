c023c8d3ea9f6b3cdd52a9a4261298c6832b0ca1
Peetz0r <peter@haas-en-berg.nl>
2022-06-19 00:10:04 +0200

    Barcode printing now automatically picks usb (at mch2022) or wifi (at revspace)
    Also has shalog-specific barcode-related ptouch-770 improvements to support the above

--- barcode.sh
+++ barcode.sh
@@ -2,11 +2,22 @@
 
 host="10.42.42.222"
 port="9100"
-width="$(curl -s http://$host/general/status.html | grep -Po '\d+(?=mm)')"
 
-echo -n "Detected $width mm tape, ";
+function code128 () {
+  zint --barcode 20 --notext --height 50 --scale 0.5 --direct --data "$1" |\
+  convert mch-logo-12mm.png PNG:- +append +antialias -gravity center -font DejaVu-Sans-Mono-Bold -pointsize 20 "label:$1" -append -gravity west -extent x128 pbm:-
+}
+
+function aztec () {
+  zint --barcode 92 --vers 4 --scale 2 --direct --data "$1" |\
+  convert mch-logo-24mm.png -append -gravity center PNG:- +antialias -font DejaVu-Sans-Mono-Bold -pointsize 20 -size 128x "caption:$1" -append -rotate 90 pbm:-
+}
 
 function checktape () {
+  echo "Trying label printer on wifi at $host"
+  width="$(curl -s http://$host/general/status.html | grep -Po '\d+(?=mm)')"
+  echo -n "Detected $width mm tape, ";
+
   if [ "$width" != "$1" ]; then
     echo -e "\033[1;91mplease load $1 mm tape instead!\033[0m";
     # do not exit, send the incorrent print command anyway
@@ -17,17 +28,25 @@ function checktape () {
   fi;
 }
 
-if [ "$1" == "code128" ]; then
-  checktape "12"
-  echo "printing Code128 barcode";
-  zint --barcode 20 --notext --height 50 --scale 0.5 --direct --data "$2" |\
-  convert mch-logo-12mm.png PNG:- +append +antialias -gravity center -font DejaVu-Sans-Mono-Bold -pointsize 20 "label:$2" -append -gravity west -extent x128 pbm:- |\
-  ptouch-770/ptouch-770-write 12 | nc $host $port;
+if lsusb | grep -iE Brother.+PT > /dev/null; then
+  echo "Detected label printer on USB"
 
-elif [ "$1" == "aztec" ]; then
-  checktape "24"
-  echo "printing Aztec barcode";
-  zint --barcode 92 --vers 4 --scale 2 --direct --data "$2" |\
-  convert mch-logo-24mm.png -append -gravity center PNG:- +antialias -font DejaVu-Sans-Mono-Bold -pointsize 20 -size 128x "caption:$2" -append -rotate 90 pbm:- |\
-  ptouch-770/ptouch-770-write 24 | nc $host $port;
-fi;
+  if [ "$1" == "code128" ]; then
+    code128 "$2" | ptouch-770/ptouch-770-write-usb 0 12
+
+  elif [ "$1" == "aztec" ]; then
+    aztec "$2" | ptouch-770/ptouch-770-write-usb 0 24
+  fi;
+
+else
+  if [ "$1" == "code128" ]; then
+    checktape "12"
+    echo "printing Code128 barcode";
+    code128 "$2" | ptouch-770/ptouch-770-write-stdout 12 | nc $host $port;
+
+  elif [ "$1" == "aztec" ]; then
+    checktape "24"
+    echo "printing Aztec barcode";
+    aztec "$2" | ptouch-770/ptouch-770-write-stdout 24 | nc $host $port;
+  fi;
+fi
