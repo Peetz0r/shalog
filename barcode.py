e1d602e0f762fbf7ada5d4f557da92670fcd4dcf
Peetz0r <peter@haas-en-berg.nl>
2025-03-04 02:14:22 +0100

    barcode.py debug mode

--- barcode.py
+++ barcode.py
@@ -65,8 +65,8 @@ def generate_code128(txt):
 
   return im
 
-def get_label_size():
-  with open('/dev/usb/lp0', 'r+b') as fp:
+def get_label_size(printer):
+  with open(printer, 'r+b') as fp:
     buf = io.BytesIO()
 
     fp.write(b'\x00'*200+b'\x1b\x40\x1b\x69\x53')
@@ -77,30 +77,37 @@ def get_label_size():
 
 if __name__ == '__main__':
   
-  if len(sys.argv) < 3:
-    print("Usage:")
-    print(f"  {sys.argv[0]} aztec foo [bar [baz [...]]]")
-    print(f"  {sys.argv[0]} code128 foo [bar [baz [...]]]")
+  debug = False
+  if '-d' in sys.argv:
+    debug = True
+    sys.argv.remove('-d')
+
+  if len(sys.argv) < 3 or sys.argv[1] not in ['aztec', 'code128']:
+    print(f'Usage: {sys.argv[0]} [-d] {{aztec|code128}} <text> ...')
+    print(f'  -d        display (instead of print) the barcode')
+    print(f'  aztec     square 2d barcode')
+    print(f'  code128   long 1d barcode')
+    print(f'  <text>    text to be encoded in the barcode')
     sys.exit(1)
-  
+
   printers = glob.glob(PRINTER_GLOB)
   if len(printers) < 1:
     print(f'Printer not found: no matches for \033[96m\033[1m{PRINTER_GLOB}\033[0m', file=sys.stderr)
-    sys.exit(2)
-    
-  printer = printers[0]
+    print('Continuing for debug') if debug else sys.exit(2)
+  else:
+    printer = printers[0]
 
-  if len(printers) > 1:
-    print(f'{len(printers)} printers found, picking \033[96m\033[1m{printer}\033[0m')
-  
-  label_size = get_label_size()
-  if label_size == 0:
-    print('Missing label tape', file=sys.stderr)
-    sys.exit(3)
-  
-  if sys.argv[1] == 'aztec' and label_size >= 18:
+    if len(printers) > 1:
+      print(f'{len(printers)} printers found, picking \033[96m\033[1m{printer}\033[0m')
+    
+    label_size = get_label_size(printer)
+    if label_size == 0:
+      print('Missing label tape', file=sys.stderr)
+      print('Continuing for debug') if debug else sys.exit(3)
+
+  if sys.argv[1] == 'aztec' and (debug or label_size >= 18):
     generate_label = generate_aztec
-  elif sys.argv[1] == 'code128' and label_size == 12:
+  elif sys.argv[1] == 'code128' and (debug or label_size == 12):
     generate_label = generate_code128
   else:
     print(f'Cannot print {sys.argv[1]} code on \033[96m\033[1m{label_size}mm\033[0m tape.', file=sys.stderr)
@@ -112,9 +119,12 @@ if __name__ == '__main__':
     tmp = tempfile.NamedTemporaryFile()
     im.save(tmp, 'PPM')
     imgs.append(tmp)
+    if debug:
+      im.show()
 
-  ptouch = subprocess.run(['./ptouch-770/ptouch-770-write', '0'] + [i.name for i in imgs], capture_output=True)
-  if ptouch.returncode != 0:
-    print(f'ptouch-770 error. Return code: \033[96m\033[1m{ptouch.returncode}\033[0m', file=sys.stderr)
-    print(f'StdErr: {ptouch.stderr.decode("ascii")}\nStdOut: {ptouch.stdout.decode("ascii")}', file=sys.stderr)
-    sys.exit(5)
+  if not debug:
+    ptouch = subprocess.run(['./ptouch-770/ptouch-770-write', '0'] + [i.name for i in imgs], capture_output=True)
+    if ptouch.returncode != 0:
+      print(f'ptouch-770 error. Return code: \033[96m\033[1m{ptouch.returncode}\033[0m', file=sys.stderr)
+      print(f'StdErr: {ptouch.stderr.decode("ascii")}\nStdOut: {ptouch.stdout.decode("ascii")}', file=sys.stderr)
+      sys.exit(5)
