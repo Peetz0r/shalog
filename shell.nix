b1f7b3b29b62a44140bc20eb0eb92f928ea15dc2
f0x <f0x@cthu.lu>
2025-08-03 12:41:01 +0200

    nix env

--- shell.nix
+++ shell.nix
@@ -1,7 +1,22 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell rec {
-	buildInputs = with pkgs; [
-    rakudo zef readline # tracking.raku
+# pkgs.mkShell rec {
+# nativeBuildInputs = with pkgs; [readline70];
+# 	buildInputs = with pkgs; [
+#     rakudo zef readline70 # tracking.raku
+#     zint # barcodes
+#     (python3.withPackages (pythonPkgs: with pythonPkgs; [
+#         paho-mqtt configparser inotify-simple # stats.py
+#         requests # odarissync.py
+#         pillow # barcode.py
+#     ]))
+#     nodejs_22 yarn # rented-overview.js
+#     cmake libudev-zero # ptouch-770
+# 	];
+# }
+(pkgs.buildFHSEnv {
+  name = "shalog-env";
+  targetPkgs = pkgs: (with pkgs; [
+    rakudo zef readline70
     zint # barcodes
     (python3.withPackages (pythonPkgs: with pythonPkgs; [
         paho-mqtt configparser inotify-simple # stats.py
@@ -10,5 +25,6 @@ pkgs.mkShell rec {
     ]))
     nodejs_22 yarn # rented-overview.js
     cmake libudev-zero # ptouch-770
-	];
-}
+  ]);
+  runScript = "zsh";
+}).env
